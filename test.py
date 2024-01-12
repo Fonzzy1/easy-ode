af49d57980f8021b57619b343d14f72a6a8b4c4f
Fonzzy1 <alfiechadwick@hotmail.com>
2024-01-12 12:31:02 +1100

    Implement harmonic motion integration and refactor
    Implement simple Euler integration for harmonic motion.
    Added example of harmonic motion to the testing script.
    Refactored the existing integration execution to maintain clarity.

--- test.py
+++ test.py
@@ -2,8 +2,16 @@ from stepping_matrix import euler
 import numpy as np
 from IVP import linear
 
+
+# Simple euler integration
 t = 0
 y = np.array([0, 10, -9.8])
-
-
 sol = linear(t, y, euler, np.identity(3))
+
+
+# Harmonic motion , y'' = -a*y
+t = 0
+y = np.array([0, 1, 0])
+a = 0.4
+T_matix = np.array([[1, 0, 0], [0, 1, 0], [-a, 0, 0]])
+sol = linear(t, y, euler, T_matix, steps=100)
